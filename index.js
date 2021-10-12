9bd02b01117b4974145e3f465faf486ea261451b
Diana <dianabacioccoart@gmail.com>
2021-10-12 15:43:53 -0700

    commit tree b50f83c75a094425decc7f70886a14b0e50d0693

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+    //return Math.abs(42 - pickupBlock)
+    if (pickupBlock <= 42) {
+        return 42 - pickupBlock
+    } else {
+        return pickupBlock - 42
+    }
+}
+
+ function distanceFromHqInFeet(pickupBlock){
+    return distanceFromHqInBlocks(pickupBlock) * 264
+ }
+
+ function distanceTravelledInFeet(start, destination){
+    //return Math.abs((start - destination)* 264)
+    if (start > destination) {
+        return (start - destination) * 264
+    } else {
+        return (destination - start) * 264
+    }
+ }
+
+ function calculatesFarePrice(start, destination){
+     const distance = distanceTravelledInFeet(start, destination)
+     if (distance < 400) {
+        return 0
+     } else if (distance > 400 && distance <= 2000) {
+         return .02 * (distance - 400)
+     } else if (distance > 2000 && distance < 2500) {
+         return 25
+     } else {
+         return 'cannot travel that far'
+     }
+
+ }
